626788516a58a1e29f6e7983f8f2692ba1aba1ff
blackstormx <blackshtormx@gmail.com>
2020-03-26 16:10:22 +0400

    model

--- Assets/CarComponent.cs
+++ Assets/CarComponent.cs
@@ -12,6 +12,10 @@ public class CarComponent : MonoBehaviour
 
     public Transform wheelMeshparent;
 
+    public float forwardTorque = 100;
+    public float brakesMax = 0.001f;
+
+    public float maxSteer = 25;
 
 
     void Start()
@@ -21,18 +25,45 @@ public class CarComponent : MonoBehaviour
             GameObject instance = Instantiate(wheelMesh, wheelCollider.transform.position, wheelMesh.transform.rotation);
             instance.transform.parent = wheelMeshparent;
             wheelObjs.Add(instance);
+            instance.transform.localScale = wheelCollider.transform.localScale;
+        }
+    }
+
+    void Update()
+    {
+        float forward = Input.GetAxis("Vertical");
+        float sideWays = Input.GetAxis("Horizontal");
+
+        float brakes = 0;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            brakes = brakesMax;
+        }
+
+        for(int i = 0; i < wheelObjs.Count; i++)
+        {
+            GameObject wheel = wheelObjs[i];
+            Wheel wc = wheelColliders[i];
+
+            Vector3 pos = wheel.transform.localPosition;
+            pos.y = wc.wheelPosition;
+            wheel.transform.localPosition = pos;
+
+            wc.brakes = brakes;
+            wc.torque = forward * forwardTorque;
+
+            if (wc.isSteer)
+            {
+                wc.steerAngle = Mathf.Clamp(Mathf.Rad2Deg * Mathf.Asin(sideWays), -maxSteer, maxSteer);
+                Vector3 rot = wheel.transform.localRotation.eulerAngles;
+                rot.y = wc.wheelAngle;
+                wheel.transform.localRotation = Quaternion.Euler(rot);
+            }
         }
     }
 
     void FixedUpdate()
     {
-        int i = 0;
-        foreach(GameObject wheel in wheelObjs)
-        {
-            Vector3 pos = wheel.transform.localPosition;
-            pos.y = wheelColliders[i].wheelPosition;
-            wheel.transform.localPosition = pos;
-            i++;
-        }
+        
     }
 }
--- Assets/Wheel.cs
+++ Assets/Wheel.cs
@@ -27,6 +27,16 @@ public class Wheel : MonoBehaviour
     public Vector3 frictionForce;
     public Vector3 forwardVelocity;
 
+    public float brakes = 1;
+
+    public float torque = 0;
+    public float RPM = 0;
+
+    public float steerAngle = 0;
+    public float wheelAngle = 0;
+    public bool isSteer;
+    public float steerSpeed = 10;
+
     void Start()
     {
         minLength = -suspensionDistance / 2;
@@ -38,12 +48,26 @@ public class Wheel : MonoBehaviour
     public float lastContactDepth = 0;
     public float contactSpeed = 0;
 
+    private Vector3 lastForwardVelocity;
+
+    void Update()
+    {
+        wheelAngle = Mathf.Lerp(wheelAngle, steerAngle, steerSpeed * Time.deltaTime);
+        transform.localRotation = Quaternion.Euler(transform.up * wheelAngle);
+    }
+
     void FixedUpdate()
     {
+        Vector3 localVelocity = car.transform.InverseTransformDirection(car.velocity);
+        Vector3 localAngularVelocity = car.transform.InverseTransformDirection(car.angularVelocity);
+
+        RPM = (localVelocity.x - lastForwardVelocity.x) / wheelRadius;
+
         RaycastHit hit;
         Debug.DrawRay(transform.position, -transform.up * wheelRadius, Color.red);
         if (Physics.Raycast(transform.position, -transform.up, out hit, wheelRadius, hitLayer))
         {
+
             lastContactDepth = contactDepth;
 
             contactDepth = Mathf.Clamp(hit.distance - wheelRadius, minLength, maxLength);
@@ -55,32 +79,35 @@ public class Wheel : MonoBehaviour
 
             wheelPosition = -contactDepth;
 
-            Vector3 localVelocity = car.transform.InverseTransformDirection(car.velocity);
-            Vector3 localAngularVelocity = car.transform.InverseTransformDirection(car.angularVelocity);
-
-           Vector3 sidewaysFrictionForce = car.transform.TransformDirection(
-               Vector3.forward *
-               -sidewaysFriction *
-               localVelocity.z
-           );
-           Vector3 forwardFrictionForce = car.transform.TransformDirection(
-                Vector3.right *
-                -forwardFriction *
-                localVelocity.x
-           );
-            Vector3 angularFrictionForce = 
+            Vector3 sidewaysFrictionForce = car.transform.TransformDirection(
+                Vector3.forward *
+                -sidewaysFriction *
+                localVelocity.z
+            );
+            Vector3 forwardFrictionForce = car.transform.TransformDirection(
+                 Vector3.right *
+                 -forwardFriction *
+                 localVelocity.x
+            ) * brakes;
+            Vector3 angularFrictionForce =
                 Vector3.forward *
                 sidewaysFriction *
                 localAngularVelocity.y
            ;
-            car.AddRelativeForce(angularFrictionForce);
+            Vector3 forwardForce = car.transform.right * torque;
+
+            Vector3 angularVeolcity = car.angularVelocity;
+            angularVeolcity.y = 0;
+            car.angularVelocity = angularVeolcity;
 
             frictionForce = sidewaysFrictionForce + forwardFrictionForce;
 
             Debug.DrawRay(transform.position, frictionForce, Color.blue);
-            
-            car.AddForceAtPosition(springForce + frictionForce, transform.position);
+
+            car.AddForceAtPosition(springForce + frictionForce + forwardForce, transform.position);
             forwardVelocity = car.velocity;
+            lastForwardVelocity = localVelocity;
+
         }
     }
 }
